7448234f9ee46865e89dc1fff669c2afcb50c23d
Devp-sriram <sriramraman100@gmail.com>
2025-03-06 22:06:27 +0530

    dynamic grid

--- components/Memory.tsx
+++ components/Memory.tsx
@@ -1,14 +1,23 @@
-import React ,{ useState } from "react"
+import React ,{ useState ,useEffect } from "react"
 
+interface cards{
+    id : Number,
+    num : Number,
+}
 
 export default function Memory() {
 
+
+
   const [gridSize,setGridSize] = useState<number>(4);
-  const [cards,setcards] = useState([]);
+  const [cards,setcards] = useState<cards[]>([]);
   const [flipped,setflipped] = useState([]);
   const [solved,setSolved] = useState<boolean>(false);
   const [won,setWon] = useState<boolean>(false);
 
+  useEffect(()=>{
+      initGame()
+  },[gridSize])
 
   function handleGridSizeChange(e :React.ChangeEvent<HTMLInputElement>){
     let size = parseInt(e.target.value)
@@ -16,7 +25,18 @@ export default function Memory() {
   }
 
   function initGame(){
-  
+    const size = gridSize*gridSize; // 16
+    const pairs = Math.floor(size/2); // 8
+    const numbers = [...Array(pairs).keys()].map((n)=> n + 1);
+    const shuffledCards = [...numbers,...numbers]
+                          .sort(()=> Math.random() - 0.5)
+                          .slice(0,size)
+                          .map((num,idx) =>({id:idx ,num}))
+
+    setcards(shuffledCards)
+    setflipped([])
+    setSolved(false)
+    setWon(false)
   }
   
   return (
@@ -37,9 +57,17 @@ export default function Memory() {
           />
       </div>
       {/*game borad*/}
-      <div>
-          
+      <div className={`grid gap-2 mb-4` }
+      style ={{
+        gridTemplateColoums : `repeat(${gridSize})`
+      }}>
+      {cards.map(card =>{
+          return <div key ={card.id}>
+                  {card.num}
+                </div>
+      })}
       </div>
+
       {/*result*/}
       {/*reset*/}
 
