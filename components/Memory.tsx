fd2e413473d30586435927cc3c54d1168590c3e9
Devp-sriram <sriramraman100@gmail.com>
2025-03-07 21:25:49 +0530

    grid styled

--- components/Memory.tsx
+++ components/Memory.tsx
@@ -1,8 +1,8 @@
-import React ,{ useState ,useEffect } from "react"
+import React ,{ useState ,useEffect, ReactNode ,Key, SetStateAction } from "react"
 
 interface cards{
-    id : Number,
-    num : Number,
+    id : Key,
+    num : ReactNode,
 }
 
 export default function Memory() {
@@ -11,8 +11,9 @@ export default function Memory() {
 
   const [gridSize,setGridSize] = useState<number>(4);
   const [cards,setcards] = useState<cards[]>([]);
-  const [flipped,setflipped] = useState([]);
-  const [solved,setSolved] = useState<boolean>(false);
+  const [flipped,setflipped] = useState<Key[]>([]);
+  const [solved,setSolved] = useState<Key[]>([]);
+  const [disabled,setDisabled] = useState<boolean>(false);
   const [won,setWon] = useState<boolean>(false);
 
   useEffect(()=>{
@@ -35,9 +36,38 @@ export default function Memory() {
 
     setcards(shuffledCards)
     setflipped([])
-    setSolved(false)
+    setSolved([])
     setWon(false)
   }
+
+  function cheackMatch(){
+    const [firstId , secondId] :Key[] = flipped
+    if(cards[firstId].num === cards[secondId].num){
+      setSolved([...solved,firstId,secondId])
+    }
+  }
+
+  function handleClick(id : Key){
+    if(disabled || won) return 
+
+    if(flipped.length <= 1){
+      if(id !== flipped[0]){
+        setflipped([...flipped,id]  as SetStateAction<typeof flipped>)
+        // match logic
+        cheackMatch()
+      }else{
+        setflipped([]);
+        setDisabled(false);
+      }
+      return 
+    }
+    
+  }
+
+  function isFlipped(id : Key): Boolean{
+      return (flipped as Key[]).includes(id)
+  }
+
   
   return (
     <div className=" flex flex-col items-center justify-center min-h-screen bg-grey-100 p-4">
@@ -57,13 +87,19 @@ export default function Memory() {
           />
       </div>
       {/*game borad*/}
-      <div className={`grid gap-2 mb-4` }
-      style ={{
-        gridTemplateColoums : `repeat(${gridSize})`
-      }}>
+      <div className={`grid gap-2 mb-4` } 
+            style={{
+              gridTemplateColumns : `repeat(${gridSize},minmax(0,1fr))`,
+              width: `min(100%,${gridSize * 5}rem)`
+            }}>
       {cards.map(card =>{
-          return <div key ={card.id}>
-                  {card.num}
+          return <div key ={card.id}
+                      onClick={()=>handleClick(card.id)}
+                      className={`aspect-square flex justify-center items-center 
+                                  text-xl font-bold rounded-lg cursor-pointer 
+                                  transition-all duration-300 
+                                  ${isFlipped(card.id)? "bg-blue-500 text-white":"bg-gray-300 text-gray-400"}`}>
+                  {isFlipped(card.id) ? card.num :"?"}
                 </div>
       })}
       </div>
